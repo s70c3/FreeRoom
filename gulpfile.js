579471b2f60635a26671c2af779e54b9c2793370
s70c3 <ms70c3@gmail.com>
2017-02-20 05:02:06 +0300

    Room Info and minify css

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var csso = require('gulp-csso'); // Минификация CSS
+var uglify = require('gulp-uglify'); // Минификация JS
 
 gulp.task('sass', function () {
     return gulp.src('./src/css/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(csso()) // минимизируем css
         .pipe(gulp.dest('./src/styles'));
 });
 
